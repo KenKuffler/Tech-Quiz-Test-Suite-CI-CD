47d6c4075ec2a4b9833dc34a669fb581a5bbeead
KenKuffler <kennethrkuffler@hotmail.com>
2024-12-02 19:06:09 -0600

    render deploy commit 7

--- server/src/seeds/seed.ts
+++ server/src/seeds/seed.ts
@@ -1,8 +1,13 @@
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import db from "../config/connection.js";
 import Question from "../models/Question.js";
 import cleanDB from "./cleanDb.js";
+
+// Derive __dirname equivalent for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Load JSON data using fs
 const pythonQuestions = JSON.parse(
