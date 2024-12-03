46b2a392dd23cf9322720de68e4168291caf72a5
KenKuffler <kennethrkuffler@hotmail.com>
2024-12-02 18:24:04 -0600

    commit of initial workflows

---
+++ dist/server/src/seeds/cleanDb.js
@@ -0,0 +1,17 @@
+import models from '../models/index.js';
+import db from '../config/connection.js';
+export default async (modelName, collectionName) => {
+    try {
+        const model = models[modelName];
+        if (!model || !model.db || !model.db.db) {
+            throw new Error(`Model or nested database properties are undefined for model: ${modelName}`);
+        }
+        const modelExists = await model.db.db.listCollections({ name: collectionName }).toArray();
+        if (modelExists.length) {
+            await db.dropCollection(collectionName);
+        }
+    }
+    catch (err) {
+        throw new Error(`Failed to clean database for ${modelName}: ${err}`);
+    }
+};
--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -3,14 +3,19 @@ import db from '../config/connection.js';
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
-    let modelExists = await models[modelName].db.db.listCollections({
-      name: collectionName
-    }).toArray()
+    const model = models[modelName];
+    if (!model || !model.db || !model.db.db) {
+      throw new Error(`Model or nested database properties are undefined for model: ${modelName}`);
+    }
+
+    const modelExists = await model.db.db.listCollections({ name: collectionName }).toArray();
 
     if (modelExists.length) {
       await db.dropCollection(collectionName);
     }
   } catch (err) {
-    throw err;
+    throw new Error(`Failed to clean database for ${modelName}: ${err}`);
   }
-}
+};
+
+
